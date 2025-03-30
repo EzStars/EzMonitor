ff26f53abba3fcda83ebe22ef7aff33b0063635a
ni0duann <2014567981@qqcom>
2025-03-30 13:43:44 +0800

    feat: 添加 ESLint 配置，更新 lint-staged 和 package.json 以支持代码检查

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  globalIgnores(["node_modules", "dist", ".turbo", "**/dist/"]),
+]);
