391d824d41d8f07dce282b09b4e490567b77e130
ni0duann <2014567981@qqcom>
2025-03-31 01:30:04 +0800

    feat: :sparkles: 添加 commit-msg 钩子以支持 Commitlint，更新 ESLint 配置以优化忽略规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,19 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
   tseslint.configs.recommended,
-  globalIgnores(["node_modules", "dist", ".turbo", "**/dist/"]),
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off', // 可以设置为 "error" 完全禁止使用
+    },
+  },
+  globalIgnores(['node_modules', '.turbo', '**/dist/']),
 ]);
