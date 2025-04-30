8d1ff084da44a7a8785649e244ed25a749012bcd
ni0duann <2014567981@qq.com>
2025-04-30 18:16:53 +0800

    feat: :sparkles: 添加初始化功能并导出监控 SDK 的核心模块

--- packages/monitor-sdk/src/index.ts
+++ packages/monitor-sdk/src/index.ts
@@ -1,2 +1,20 @@
-export { Monitor } from './core/monitor';
-export * from './core/types';
+import { setConfig } from './common/config';
+import { ConfigType } from './types';
+import Behavior, { getBehaviour } from './plugins/behavior';
+import Performance from './plugins/performance';
+
+import { unzipRecordscreen } from './common/utils';
+
+function init(options?: ConfigType) {
+  setConfig(options);
+  window.$SDK = {};
+}
+
+export default {
+  init,
+  Performance,
+  Error,
+  Behavior,
+  unzipRecordscreen,
+  getBehaviour,
+};
