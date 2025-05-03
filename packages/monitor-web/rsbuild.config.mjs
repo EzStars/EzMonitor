e17bca88b624da7818ee5dea29d283fab36b7c4b
ni0duann <2014567981@qq.com>
2025-05-03 19:11:43 +0800

    feat: :sparkles: 初始化monitor-web项目

---
+++ packages/monitor-web/rsbuild.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginBabel } from '@rsbuild/plugin-babel';
+
+export default defineConfig({
+  source: {
+    entry: {
+      index: './src/index.tsx',
+    },
+  },
+  html: {
+    template: './public/index.html',
+  },
+  plugins: [
+    pluginBabel({
+      include: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      babelLoaderOptions: {
+        presets: [
+          ['@babel/preset-react', { 
+            runtime: 'automatic',
+            importSource: 'react'
+          }],
+          '@babel/preset-env',
+          '@babel/preset-typescript'
+        ]
+      }
+    })
+  ]
+});
