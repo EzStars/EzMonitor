f16af7b8d7c6300e156edaedb2d842c600cafa7c
ni0duann <2014567981@qq.com>
2025-05-04 14:37:52 +0800

    feat: :sparkles: 添加React Router配置并更新相关页面

--- packages/monitor-web/main.tsx
+++ packages/monitor-web/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './src/app';
+import router from './src/router';
+import { RouterProvider } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}></RouterProvider>,
 );
